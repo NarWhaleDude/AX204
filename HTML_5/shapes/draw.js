ffbda0f73a8f02954ac0eb878514f9322c93601a
Jacinta Sutton <jacintasutton@Jacintas-MacBook-Pro.local>
2016-09-10 10:00:38 +0800

    lesson 2

---
+++ HTML_5/shapes/draw.js
@@ -0,0 +1,36 @@
+console.log("working");
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
+
+ctx.fillStyle = "rgb(255, 150, 200)";
+ctx.fillRect(75, 75, 150, 150);
+ctx.clearRect(120, 120, 60, 60);
+
+
+
+
+
+
+
+var x = 0;
+var y = 0;
+var width = 300;
+var height = 300;
+ctx.fillRect(x,y,width,height);
+while (width > 0) {
+	x += 10;
+	y += 10;
+	width -= 20;
+	height -= 20;
+	ctx.clearRect(x,y,width,height);
+	x += 10;
+	y += 10;
+	width -= 20;
+	height -= 20;
+	ctx.fillRect(x,y,width,height);
+}
