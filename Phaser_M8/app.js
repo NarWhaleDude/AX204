784bbfe808e6f4e9068f642cd6956d17d9e3ffd9
Jacinta Sutton <jacintasutton@Jacintas-MacBook-Pro.local>
2016-11-17 18:20:14 +0800

    Phaser stuff

--- Phaser_M8/app.js
+++ Phaser_M8/app.js
@@ -8,6 +8,7 @@ function preload(){
     game.load.image("sky", "assets/sky.png");
     game.load.image("ground", "assets/platform.png");
     game.load.image("star", "assets/star.png");
+    game.load.sprite("dude", "assets/dude.png", 32, 48);
 }
 
 function create(){
